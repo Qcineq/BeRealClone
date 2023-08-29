40e2e980932fad8390c00085e15e891411b786e2
Kuba <jakub.kucinski@icloud.com>
2023-08-29 09:05:22 +0200

    Initial Commit

---
+++ BeRealClone/BeRealCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeRealCloneApp.swift
+//  BeRealClone
+//
+//  Created by Jakub Kuciński on 29/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct BeRealCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
