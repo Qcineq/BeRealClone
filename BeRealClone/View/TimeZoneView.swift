900b6c79ce710d4dbed0bf56ac9ffbf74ab91408
Kuba <jakub.kucinski@icloud.com>
2023-09-06 23:22:20 +0200

    Created TimeZoneView and updated names of view files

---
+++ BeRealClone/View/TimeZoneView.swift
@@ -0,0 +1,20 @@
+//
+//  TimeZoneView.swift
+//  BeRealClone
+//
+//  Created by Jakub Kuciński on 06/09/2023.
+//
+
+import SwiftUI
+
+struct TimeZoneView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TimeZoneView_Previews: PreviewProvider {
+    static var previews: some View {
+        TimeZoneView()
+    }
+}
