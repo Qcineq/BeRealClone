a68cbae325e4eeb10d23d341ae308a530562d574
Kuba <jakub.kucinski@icloud.com>
2023-09-10 12:35:50 +0200

    added buttons to choose view in left menu view

--- BeRealClone/View/LeftMenuView.swift
+++ BeRealClone/View/LeftMenuView.swift
@@ -8,6 +8,11 @@
 import SwiftUI
 
 struct LeftMenuView: View {
+    
+    @State var width = UIScreen.main.bounds.width
+    
+    @State var menu = "suggestions"
+    
     var body: some View {
         VStack {
             ZStack {
@@ -15,7 +20,76 @@ struct LeftMenuView: View {
                 
                 LeftMenuTopView()
                 
-                Suggestions()
+                if menu == "suggestions" {
+                    Suggestions()
+                } else if menu == "friends" {
+                    Friends()
+                } else if  menu == "requests" {
+                    Requests()
+                }
+                
+                VStack {
+                    
+                    Spacer()
+                    
+                    ZStack {
+                        VStack {
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 22)
+                                    .frame(width: width * 0.7, height: 40)
+                                    .foregroundColor(Color(red: 20/255, green: 20/255, blue: 20/255))
+                                
+                                HStack(spacing: 4) {
+                                    Capsule()
+                                        .frame(width: 100, height: 28)
+                                        .foregroundColor(Color(red: 46/255, green: 46/255, blue: 48/255))
+                                        .opacity(menu == "suggestions" ? 1 : 0)
+                                        .overlay(
+                                            Text("Suggestions")
+                                                .foregroundColor(.white)
+                                                .font(.system(size: 14))
+                                        )
+                                        .onTapGesture {
+                                            self.menu = "suggestions"
+                                        }
+                                    
+                                    Capsule()
+                                        .frame(width: 70, height: 28)
+                                        .foregroundColor(Color(red: 46/255, green: 46/255, blue: 48/255))
+                                        .opacity(menu == "friends" ? 1 : 0)
+                                        .overlay(
+                                            Text("Friends")
+                                                .foregroundColor(.white)
+                                                .font(.system(size: 14))
+                                        )
+                                        .onTapGesture {
+                                            self.menu = "friends"
+                                        }
+                                    
+                                    Capsule()
+                                        .frame(width: 75, height: 28)
+                                        .foregroundColor(Color(red: 46/255, green: 46/255, blue: 48/255))
+                                        .opacity(menu == "requests" ? 1 : 0)
+                                        .overlay(
+                                            Text("Requests")
+                                                .foregroundColor(.white)
+                                                .font(.system(size: 14))
+                                        )
+                                        .onTapGesture {
+                                            self.menu = "requests"
+                                        }
+                                }
+                            }
+                        }
+                        .zIndex(1)
+                        
+                        LinearGradient(colors: [.black, .white.opacity(0)], startPoint: .bottom, endPoint: .top)
+                            .ignoresSafeArea()
+                            .frame(height: 60)
+                            .opacity(0.9)
+                        
+                    }
+                }
             }
         }
     }
