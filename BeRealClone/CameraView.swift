f6d394385f0ea2f6e50283cc44e6520f56e7173a
Kuba <jakub.kucinski@icloud.com>
2024-01-20 11:22:12 +0100

    Added temporary functionality for selecting a photo from the gallery and displaying it in cameraView as front image

--- BeRealClone/CameraView.swift
+++ BeRealClone/CameraView.swift
@@ -13,9 +13,15 @@ struct CameraView: View {
     
     @State var takePhotoClick = false
     
-    @State var selectedBackImage: UIImage?
+    @State var chooseFrontPhoto = false
     
+    @State var photoTaken = false
+    
+    @State var selectedBackImage: UIImage?
     @State var backImage: Image?
+    
+    @State var selectedFrontImage: UIImage?
+    @State var frontImage: Image?
     
     var body: some View {
         VStack {
@@ -40,60 +46,119 @@ struct CameraView: View {
                         .font(.system(size: 20))
                         .fontWeight(.heavy)
                     
-                    if let image = backImage {
-                        image
-                            .resizable()
-                            .cornerRadius(12)
-                            .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.6)
-                            .scaledToFit()
-                            .overlay(
-                                
-                                VStack {
-                                    ProgressView()
-                                    Text("Wait, wait, wait, now smile")
+                    ZStack(alignment: .topLeading) {
+                        GeometryReader { g in
+                            VStack {
+                                if frontImage != nil {
+                                    HStack {
+                                        RoundedRectangle(cornerRadius: 10)
+                                            .frame(width: 125, height: 165)
+                                            .overlay(
+                                                
+                                                VStack {
+                                                    if let imageFront = frontImage {
+                                                        imageFront
+                                                            .resizable()
+                                                            .cornerRadius(8)
+                                                            .frame(width: 120, height: 160)
+                                                            .scaledToFit()
+                                                    }
+                                                }
+                                            )
+                                            .padding(.leading)
+                                        Spacer()
+                                    }
+                                    .padding(.top, 18)
                                 }
-                                    .foregroundStyle(.white)
-                                    .opacity(self.switchingCamera ? 1 : 0)
-                            )
-                        
-                    } else {
-                        RoundedRectangle(cornerRadius: 12)
-                            .foregroundColor(.gray)
-                            .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.6)
-                            .overlay(
-                                
-                                VStack {
-                                    ProgressView()
-                                    Text("Wait, wait, wait, now smile")
-                                }
-                                    .foregroundStyle(.white)
-                                    .opacity(self.switchingCamera ? 1 : 0)
-                            )
-                            .sheet(isPresented: $takePhotoClick) {
-                                loadBackImage()
-                                
-                            } content: {
-                                ImagePicker(image: $selectedBackImage)
+                                Spacer()
                             }
+                            .sheet(isPresented: $chooseFrontPhoto) {
+                                loadFrontImage()
+                                self.photoTaken.toggle()
+                                self.switchingCamera.toggle()
+                            } content: {
+                                ImagePicker(image: $selectedFrontImage)
+                            }
+                        }
+                        .zIndex(1)
+                        if let image = backImage {
+                            image
+                                .resizable()
+                                .cornerRadius(12)
+                                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.6)
+                                .scaledToFit()
+                                .overlay(
+                                    
+                                    VStack {
+                                        ProgressView()
+                                        Text("Wait, wait, wait, now smile")
+                                    }
+                                        .foregroundStyle(.white)
+                                        .opacity(self.switchingCamera ? 1 : 0)
+                                )
+                            
+                        } else {
+                            RoundedRectangle(cornerRadius: 12)
+                                .foregroundColor(.gray)
+                                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height * 0.6)
+                                .overlay(
+                                    
+                                    VStack {
+                                        ProgressView()
+                                        Text("Wait, wait, wait, now smile")
+                                    }
+                                        .foregroundStyle(.white)
+                                        .opacity(self.switchingCamera ? 1 : 0)
+                                )
+                                .sheet(isPresented: $takePhotoClick) {
+                                    loadBackImage()
+                                    self.switchingCamera.toggle()
+                                    DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+                                        self.chooseFrontPhoto.toggle()
+                                    }
+                                    
+                                } content: {
+                                    ImagePicker(image: $selectedBackImage)
+                                }
+                        }
+                        
                     }
                     
                     VStack {
-                        HStack(alignment: .center, spacing: 18) {
-                            Image(systemName: "bolt.slash.fill")
-                                .font(.system(size: 28))
-                            
-                            Button {
-                                self.takePhotoClick.toggle()
-                            } label: {
-                                Image(systemName: "circle")
-                                    .font(.system(size: 70))
+                        if photoTaken {
+                            Button(action: {
+                                send()
+                            }, label: {
+                                HStack {
+                                    Text("SEND")
+                                        .foregroundStyle(.white)
+                                        .font(.system(size: 40, weight: .black))
+                                    Image(systemName: "location.north.fill")
+                                        .foregroundStyle(.white)
+                                        .font(.system(size: 20, weight: .black))
+                                        .rotationEffect(.degrees(90))
+                                }
+                            })
+                        } else {
+                            HStack(alignment: .center, spacing: 18) {
+                                Image(systemName: "bolt.slash.fill")
+                                    .font(.system(size: 28))
+                                
+                                Button {
+                                    self.takePhotoClick.toggle()
+                                } label: {
+                                    Image(systemName: "circle")
+                                        .font(.system(size: 70))
+                                }
+                                
+                                Image(systemName: "arrow.triangle.2.circlepath")
+                                    .font(.system(size: 24))
                             }
+                            .foregroundColor(.white)
                             
-                            Image(systemName: "arrow.triangle.2.circlepath")
-                                .font(.system(size: 24))
                         }
-                        .foregroundColor(.white)
                     }
+                    .offset(y: -20)
                     
                     Spacer()
                 }
@@ -106,6 +171,16 @@ struct CameraView: View {
         guard let selectedBackImage = selectedBackImage else { return }
         backImage = Image(uiImage: selectedBackImage)
     }
+    
+    func loadFrontImage() {
+        guard let selectedFrontImage = selectedFrontImage else { return }
+        frontImage = Image(uiImage: selectedFrontImage)
+    }
+    
+    func send() {
+        print("All sent")
+    }
+    
 }
 
 #Preview {
